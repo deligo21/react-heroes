16baa6a5479aef74fbc68b87f22cd5b78854bd6f
deligo21 <arrp1239@gmail.com>
2023-03-27 23:27:24 -0400

    fixes solved

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -16,7 +16,7 @@ export const HeroCard = ({
     characters,
 }) => {
 
-    const heroImageUrl = `./heroes/${id}.jpg`;
+    const heroImageUrl = `./assets/heroes/${id}.jpg`;
 
     return (
         <div className="col animate__animated animate__fadeIn">
